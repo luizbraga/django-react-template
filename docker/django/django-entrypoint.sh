3a462a028857a083b76497eefedf019d58251bca
Luiz Braga <luizbragaa@gmail.com>
2018-03-28 01:33:07 -0300

    Dockerization

---
+++ docker/django/django-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+until cd webserver
+do
+    echo "Waiting for django volume"
+done
+
+until python manage.py migrate --settings=project_name.settings.docker
+do
+    echo "Waiting for postgres..."
+    sleep 2
+done
+
+python manage.py runserver 0.0.0.0:8000 --settings=project_name.settings.docker
